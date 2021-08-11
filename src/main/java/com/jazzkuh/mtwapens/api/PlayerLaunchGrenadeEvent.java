4309f0d6df4cd23f6757b9aa5488da138d3c6f61
Jazzkuh <32953684+jazzkuh@users.noreply.github.com>
2021-08-11 10:46:26 +0200

    Improve api for javadocs
    Signed-off-by: Jazzkuh <unconfigured@null.spigotmc.org>

--- src/main/java/com/jazzkuh/mtwapens/api/PlayerLaunchGrenadeEvent.java
+++ src/main/java/com/jazzkuh/mtwapens/api/PlayerLaunchGrenadeEvent.java
@@ -1,22 +1,43 @@
 package com.jazzkuh.mtwapens.api;
 
 import com.jazzkuh.mtwapens.function.objects.Grenade;
-import lombok.Getter;
-import lombok.Setter;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
 public final class PlayerLaunchGrenadeEvent extends Event implements Cancellable {
-    private final @Getter Grenade grenade;
-    private final @Getter Player shooter;
-    private @Getter @Setter boolean cancelled;
-    private final @Getter HandlerList handlers = new HandlerList();
+    private final Grenade grenade;
+    private final Player shooter;
+    private boolean cancelled;
+    private final HandlerList handlers = new HandlerList();
 
     public PlayerLaunchGrenadeEvent(Player shooter, Grenade grenade) {
         this.grenade = grenade;
         this.shooter = shooter;
         this.cancelled = false;
+    }
+
+    public Player getShooter() {
+        return shooter;
+    }
+
+    public Grenade getGrenade() {
+        return grenade;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    @Override
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
     }
 }
