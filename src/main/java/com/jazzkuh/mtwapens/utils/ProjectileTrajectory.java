09c726b8ddf56aa331b06c238088b9f65d05b9c6
Jazzkuh <jazzbauer77@gmail.com>
2021-08-13 16:36:24 +0200

    Forgot ProjectileTrajectory.java lol

---
+++ src/main/java/com/jazzkuh/mtwapens/utils/ProjectileTrajectory.java
@@ -0,0 +1,34 @@
+package com.jazzkuh.mtwapens.utils;
+
+import com.jazzkuh.mtwapens.Main;
+import lombok.Getter;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
+import org.bukkit.util.Vector;
+
+public class ProjectileTrajectory {
+    private final @Getter Entity entity;
+    private final @Getter Vector vector;
+
+    public ProjectileTrajectory(Entity projectile, Vector startingVector) {
+        this.entity = projectile;
+        this.vector = startingVector;
+    }
+
+    private int taskId;
+
+    public void cancelTask() {
+        Bukkit.getScheduler().cancelTask(this.taskId);
+    }
+
+    public void start() {
+        this.taskId = Bukkit.getScheduler().scheduleSyncRepeatingTask(Main.getInstance(), () -> {
+            if (!entity.isValid() || !entity.isOnGround()) {
+                Bukkit.getScheduler().cancelTask(taskId);
+                return;
+            }
+
+            entity.setVelocity(vector);
+        }, 0, 2);
+    }
+}
